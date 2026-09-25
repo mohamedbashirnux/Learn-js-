bc7e03d7a5dfa34e695e0de3fd0380d23ee30b7f
Mohamed-Amiin Bashir <mohamedbashirnuh@gmail.com>
2026-09-25 21:49:40 +0300

    added back project but has a error

---
+++ LEVEL1/projects/mini-bank/mini.js
@@ -0,0 +1,10 @@
+// mini back calcular
+
+let age = Number(prompt("Enter your age?"));
+let bankkCard = prompt("Do you have Bank card?");
+let pin = Number(prompt("Enter your bin?"));
+let Balance = Number("Enter your balance?");
+let amount = number ("Enter amount you need with drawal");
+
+let result = age => 18 && bankCard === "yes" && pin === 1234 && amount <= Balance ? "withdrwal sucsefully" : "you can nit wuthdraw"
+alert("result")
