dc96174fd604d3892b651085bfc5350523b358d7
Mohamed-Amiin Bashir <mohamedbashirnuh@gmail.com>
2026-09-25 21:12:07 +0300

    i am updated the project data

--- LEVEL1/projects/club_entery/club.js
+++ LEVEL1/projects/club_entery/club.js
@@ -1,8 +1,6 @@
-// Club entry checker
+// Club entry check.
 
-let age = Number(prompt("Enter your age:"));
-let hasID = prompt("Do you have an ID? (yes/no)");
-
-let result = (age >= 18 && hasID === "yes") ? "You can enter the club" : "You cannot enter the club";
-
-alert(result);
+// ask the user to enter his age ,  if age are greter than or equal 18?  ask another time to enter his id
+let age =Number("Enter you are age!?");
+let hasID = prompt("enter your id (yes or no)");
+let result = age >=18 && hasID ==="yes" ? "You can enter the club" : "you can not enter the club"
