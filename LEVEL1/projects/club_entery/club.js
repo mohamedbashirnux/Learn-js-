3a473dc1360ce922668a60340e08ed33c8b801c2
Mohamed-Amiin Bashir <mohamedbashirnuh@gmail.com>
2026-09-25 21:17:01 +0300

    i am fix the bug

--- LEVEL1/projects/club_entery/club.js
+++ LEVEL1/projects/club_entery/club.js
@@ -1,6 +1,7 @@
 // Club entry check.
 
 // ask the user to enter his age ,  if age are greter than or equal 18?  ask another time to enter his id
-let age =Number("Enter you are age!?");
+let age = Number(prompt("Enter you are age:"))
 let hasID = prompt("enter your id (yes or no)");
 let result = age >=18 && hasID ==="yes" ? "You can enter the club" : "you can not enter the club"
+alert(result)
